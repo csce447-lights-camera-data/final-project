71041d93e10aec458ea1b4fc30ffc88fd42883ba
mrllpz <mrllpz@tamu.edu>
2021-11-17 10:39:33 -0800

    removed scrollers, collapse detail view

--- scripts/chart.js
+++ scripts/chart.js
@@ -4,7 +4,7 @@ let series;
 
 anychart.onDocumentReady(() => {
   anychart.data.loadJsonFile("imdb.json", (data) => {
-    var stage = anychart.graphics.create('graph-container');
+    // var stage = anychart.graphics.create('graph-container');
 
     let datalist = [];
 
@@ -54,7 +54,7 @@ anychart.onDocumentReady(() => {
     // enable the legend
     chart.legend(false);
 
-    chart.container(stage).draw();
+    // chart.container(stage).draw();
 
     // set the titles of the axes
 
@@ -73,48 +73,48 @@ anychart.onDocumentReady(() => {
 
 
     // scalable axes
-    chart.margin({ left: 10, bottom: 10 });
+    // chart.margin({ left: 10, bottom: 10 });
 
-    // chart.interactivity().zoomOnMouseWheel(true);
-    var bounds = chart.getPixelBounds();
+    // // chart.interactivity().zoomOnMouseWheel(true);
+    // var bounds = chart.getPixelBounds();
 
-    //create x-scroller
-    var xScroller = anychart.standalones.scroller();
-    xScroller.parentBounds(60, bounds.height - 60, bounds.width - 80, 50);
-    xScroller.container(stage).draw();
+    // //create x-scroller
+    // var xScroller = anychart.standalones.scroller();
+    // xScroller.parentBounds(60, bounds.height - 60, bounds.width - 80, 50);
+    // xScroller.container(stage).draw();
 
-    //create y-scroller
-    var yScroller = anychart.standalones.scroller();
-    yScroller.orientation('left');
-    yScroller.parentBounds(5, 10, 0, bounds.height - 60);
-    yScroller.container(stage).draw();
+    // //create y-scroller
+    // var yScroller = anychart.standalones.scroller();
+    // yScroller.orientation('left');
+    // yScroller.parentBounds(5, 10, 0, bounds.height - 60);
+    // yScroller.container(stage).draw();
 
-    //place scrollers on window resize
-    window.onresize = (event) => {
-      var bounds = chart.getPixelBounds();
-      xScroller.parentBounds(60, bounds.height - 60, bounds.width - 80, 50);
-      yScroller.parentBounds(5, 10, 0, bounds.height - 60);
-    };
+    // //place scrollers on window resize
+    // window.onresize = (event) => {
+    //   var bounds = chart.getPixelBounds();
+    //   xScroller.parentBounds(60, bounds.height - 60, bounds.width - 80, 50);
+    //   yScroller.parentBounds(5, 10, 0, bounds.height - 60);
+    // };
 
-    //get info about scales
-    var xScale = chart.xScale();
-    var yScale = chart.yScale();
+    // //get info about scales
+    // var xScale = chart.xScale();
+    // var yScale = chart.yScale();
 
-    var minX = xScale.minimum();
-    var maxX = xScale.maximum();
-    var minY = yScale.minimum();
-    var maxY = yScale.maximum();
+    // var minX = xScale.minimum();
+    // var maxX = xScale.maximum();
+    // var minY = yScale.minimum();
+    // var maxY = yScale.maximum();
 
-    //scroller listeners and handlers
-    xScroller.listen('scrollerchange', (e) => {
-      xScale.minimum(e.startRatio * maxX + minX);
-      xScale.maximum(e.endRatio * maxX);
-    });
+    // //scroller listeners and handlers
+    // xScroller.listen('scrollerchange', (e) => {
+    //   xScale.minimum(e.startRatio * maxX + minX);
+    //   xScale.maximum(e.endRatio * maxX);
+    // });
 
-    yScroller.listen('scrollerchange', (e) => {
-      yScale.minimum(e.startRatio * maxY + minY);
-      yScale.maximum(e.endRatio * maxY);
-    });
+    // yScroller.listen('scrollerchange', (e) => {
+    //   yScale.minimum(e.startRatio * maxY + minY);
+    //   yScale.maximum(e.endRatio * maxY);
+    // });
 
     chart.listen('pointClick', (e) => {
       var index = e.iterator.getIndex();
@@ -122,15 +122,28 @@ anychart.onDocumentReady(() => {
       displayResult(row);
     })
 
-    // chart.container("graph-container");
+    chart.container("graph-container");
 
-    // // initiate drawing the chart
-    // chart.draw();
+    // initiate drawing the chart
+    chart.draw();
   });
 });
 
+const collapseResult = () => {
+  document.getElementById("result-wrapper").removeChild(document.getElementById("result"));
+  document.getElementById("result-wrapper").className = "no-result";
+}
+
 const displayResult = (movie) => {
+  if (document.body.contains(document.getElementById("result"))) {
+    collapseResult();
+  }
+  let result = document.createElement('div');
+  result.id = "result";
+  document.getElementById('result-wrapper').appendChild(result);
+  document.getElementById("result-wrapper").className = "result-display";
   const res = '<div align="center">' +
+    '<span id="close" style="cursor: pointer; float: right">&#10006</span>' +
     '<h2>' + movie.movie + '</h2>' +
     '<table style="width=100%">' +
     '<tr><td style="width: 30%"><h3>Summary</h3></td><td><h3>' + movie.summary + '</h3></td></tr>' +
@@ -142,8 +155,8 @@ const displayResult = (movie) => {
     '<tr><td style="width: 30%"><h3>Writer</h3></td><td><h3>' + (movie.writer).split(', ').join('<br/>') + '</h3></td></tr>' +
     '<tr><td style="width: 30%"><h3>Actors</h3></td><td><h3>' + (movie.actors).split(', ').join('<br/>') + '</h3></td></tr>' +
     '</table></div>';
-  document.getElementById("result").innerHTML = res;
-
+    document.getElementById("result").innerHTML = res;
+    document.getElementById("close").addEventListener("click", collapseResult);
 }
 
 const titleInput = document.getElementById("search-input--title");
